ee040a83ed1424a21524fa77be0e375d02ea5d9f
nitamon314 <nitamon314@gmail.com>
2017-01-24 01:33:02 +0000

    アソシエーション完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :tweets
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  mount_uploader :image, UserImageUploader 
+  mount_uploader :image, UserImageUploader
+  
+  validates :name, presence: true
+  validates :profile, presence: true
 end
